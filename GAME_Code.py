38499c570982d1a60a4f17056cf0509dfb87014c
Sukhman75 <sukhmansingh75@gmail.com>
2018-10-30 15:46:07 -0400

    Changes made to GAME_Code.py

--- GAME_Code.py
+++ GAME_Code.py
@@ -1,4 +1,8 @@
 import random as rd
 print("Lets Play Stone-Paper-Scissors....")
-
+print("***")
+print("***")
+print("***")
+print("***")
+print("***")
 print("Hey 'Player' I am Mr. Computer!! \n ...Make your Move...")
